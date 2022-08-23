b46237f6bb3b77caee635632f50a0ba7e64c1948
Ankush263 <ankushbanik263@gmail.com>
2022-08-23 15:06:22 +0530

    update DappToken.sol

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,3 +1,16 @@
-//SPDX-License-identifier
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract DANKUSH is ERC20 {
+  constructor() ERC20("DANKUSH", "DANK") {
+    _mint(msg.sender, 10000 * (10 ^ 18));
+  }
+
+  function faucet(address payable to, uint256 amount) payable external {
+    _mint(to, amount);
+  }
+}
+
+//12:56:8
