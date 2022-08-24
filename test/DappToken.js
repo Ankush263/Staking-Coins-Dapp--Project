d32c8551f7f751a5770cdfef775d0137887f5810
Ankush263 <ankushbanik263@gmail.com>
2022-08-24 11:11:34 +0530

    update DappToken test cases

--- test/DappToken.js
+++ test/DappToken.js
@@ -10,15 +10,14 @@ describe("Deployment", async () => {
 
   beforeEach(async () => {
     DappToken = await ethers.getContractFactory('DappToken');
-    // Staking = await ethers.getContractFactory('Staking');
-    [owner, address1, address2, ...address] = await ethers.getSigners();
+    [owner, address1] = await ethers.getSigners();
     contract = await DappToken.deploy();
   })
 
   describe("Deployment", async () => {
 
     it("Should set the Total Supply", async () => {
-      expect((await contract.totalSupply()).toNumber()).to.equal(10000 * (10 ^ 18))
+      expect((await contract.totalSupply()).toNumber()).to.equal(10000 * (10 ^ 18));
     })
 
   })
@@ -26,7 +25,19 @@ describe("Deployment", async () => {
   describe("Token Transfer", async () => {
 
     it("Should transfer the token from owner", async () => {
-      
+      await contract.transfer(address1.address, 100);
+      expect((await contract.balanceOf(address1.address)).toNumber()).to.equal(100);
     })
+
   })
+
+  describe("Token faucet", async () => {
+    
+    it("Should give some tokens", async () => {
+      await contract.faucet(address1.address, 1342);
+      expect((await contract.balanceOf(address1.address)).toNumber()).to.equal(1342);
+    })
+    
+  })
+
 })
