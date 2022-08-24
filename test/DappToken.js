053d3beda3f75312b9d205bcff88dbe6b2e611e6
Ankush263 <ankushbanik263@gmail.com>
2022-08-24 10:38:40 +0530

    Add some tests

--- test/DappToken.js
+++ test/DappToken.js
@@ -1,15 +1,32 @@
 const { expect } = require('chai');
-const { ethers } = require('ethers');
 
 describe("Deployment", async () => {
+
   let owner;
   let address1;
   let address2;
   let address;
+  let contract;
+
   beforeEach(async () => {
     DappToken = await ethers.getContractFactory('DappToken');
-    Staking = await ethers.getContractFactory('Staking');
+    // Staking = await ethers.getContractFactory('Staking');
     [owner, address1, address2, ...address] = await ethers.getSigners();
     contract = await DappToken.deploy();
+  })
+
+  describe("Deployment", async () => {
+
+    it("Should set the Total Supply", async () => {
+      expect((await contract.totalSupply()).toNumber()).to.equal(10000 * (10 ^ 18))
+    })
+
+  })
+
+  describe("Token Transfer", async () => {
+
+    it("Should transfer the token from owner", async () => {
+      
+    })
   })
 })
